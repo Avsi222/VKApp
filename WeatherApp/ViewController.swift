8a85556e8c3845afca65b458a2996b0e07a1f34f
Avsi222 <arseniydor@yandex.ru>
2018-09-20 08:27:45 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Арсений Дорогин on 20.09.2018.
+//  Copyright © 2018 AVSI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
